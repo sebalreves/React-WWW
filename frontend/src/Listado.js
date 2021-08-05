af702b68940ab7ade813cbb5d7a2c73e0dad7c20
daaguile <diego.aguileraa@sansano.usm.cl>
2021-08-04 22:34:19 -0400

    Registrar encuesta v2

--- frontend/src/Listado.js
+++ frontend/src/Listado.js
@@ -1,49 +1,52 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
-import {Grid} from "@material-ui/core";
+import {Card, CardContent, CardHeader, Grid} from "@material-ui/core";
 
 const encuestasDummy = [
-  { id: "titulo1", title: "titulo1", description: "descripcion1" },
-  { id: "titulo2", title: "titulo2", description: "descripcion2" },
-  { id: "titulo3", title: "titulo3", description: "descripcion3" },
+    {id: "titulo1", title: "titulo1", description: "descripcion1"},
+    {id: "titulo2", title: "titulo2", description: "descripcion2"},
+    {id: "titulo3", title: "titulo3", description: "descripcion3"},
 ];
 const Listado = () => {
-  const [encuestas, setEncuestas] = useState([]);
-  const [status, setStatus] = useState("idle");
+    const [encuestas, setEncuestas] = useState([]);
+    const [status, setStatus] = useState("idle");
 
-  const getEncuestas = () => {
-    console.log("CONSULTANDO ENCUESTAS");
-    setStatus("loading");
-    setTimeout(() => {
-      setEncuestas(encuestasDummy);
-      setStatus("success");
-    }, 1000);
-  };
+    const getEncuestas = () => {
+        console.log("CONSULTANDO ENCUESTAS");
+        setStatus("loading");
+        setTimeout(() => {
+            setEncuestas(encuestasDummy);
+            setStatus("success");
+        }, 1000);
+    };
 
-  //cargar listado al cargar la pagina
-  useEffect(() => {
-    console.log("USEEFFECT");
-    getEncuestas();
-  }, []);
+    //cargar listado al cargar la pagina
+    useEffect(() => {
+        console.log("USEEFFECT");
+        getEncuestas();
+    }, []);
 
-  if (status === "idle" || status === "loading") {
-    return <p>Cargando</p>;
-  }
-  return (
-    <Grid item xs="6">
-      <div className="listado">
-        {encuestas.map((encuesta) => {
-          return (
-            <>
-              <p>{encuesta.title}</p>
-              <p>{encuesta.description}</p>
-            </>
-          );
-        })}
-        <button onClick={getEncuestas}>Refrescar listado</button>
-      </div>
-    </Grid>
-  );
+    if (status === "idle" || status === "loading") {
+        return <p>Cargando</p>;
+    }
+    return (
+        <Grid item xs="6">
+            <div className="listado">
+                {encuestas.map((encuesta) => {
+                    return (
+                        <Card >
+                            <CardHeader title={encuesta.title}/>
+                            <CardContent>
+                                <p>{encuesta.description}</p>
+                            </CardContent>
+
+                        </Card>
+                    );
+                })}
+                <button onClick={getEncuestas}>Refrescar listado</button>
+            </div>
+        </Grid>
+    );
 };
 
 export default Listado;
